4a0451c3d7180e3e4df1d810ea688abd9c3fa2c1
OllyHard <pelmeni.95@mail.ru>
2020-09-21 00:29:02 +0700

    redaction

--- BmiService.java
+++ BmiService.java
@@ -3,8 +3,8 @@ package BmiService;
 public class BmiService {
     public double calculate(int weight, double growth) {
         double BMI = weight / (growth * growth);
-        {
+
             return BMI;
-        }
+
     }
 }
